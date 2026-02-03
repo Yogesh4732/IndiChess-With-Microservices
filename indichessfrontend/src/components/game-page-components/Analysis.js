bfbe461c3fc3ce0811a249bb536d332241e8ad1d
Yogesh Kumar <yogesh.sk4732@gmail.com>
2026-02-03 10:07:19 +0530

    updated resign and draw

--- indichessfrontend/src/components/game-page-components/Analysis.js
+++ indichessfrontend/src/components/game-page-components/Analysis.js
@@ -1,31 +1,11 @@
 import React from "react";
-import { useState } from "react";
 import Moves from "./Moves";
-import GameControls from "./GameControls";
 
-const Analysis = ({moves}) => {
-
-  const [gameStatus, setGameStatus] = useState("");  // Track the game status (e.g., "Resigned", "Drawn")
-
-
-  // Function to handle resign
-  const handleResign = () => {
-    setGameStatus("Resigned");
-    alert("You have resigned from the game.");
-  };
-
-  // Function to handle draw proposal
-  const handleDraw = () => {
-    setGameStatus("Draw Proposed");
-    alert("You have proposed a draw.");
-  };
-
+const Analysis = ({ moves }) => {
   return (
     <div className="moves-container">
-        <Moves moves={moves} />
-        <GameControls onResign={handleResign} onDraw={handleDraw} />  {/* Add the GameControls below Moves */}
-        {gameStatus && <p className="game-status">{gameStatus}</p>}
-      </div>
+      <Moves moves={moves} />
+    </div>
   );
 };
 
